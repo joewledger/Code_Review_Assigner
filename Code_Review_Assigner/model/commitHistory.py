c0b90238ed53a6ba43af0319d9d9485608dc408a
joewledger <joewledger@gmail.com>
2016-10-18 19:55:14 -0400

    Changes to basic commit history data structure. Since we know what branch each commit is part of, we no longer need to keep a commit history for the tree
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Code_Review_Assigner/model/commitHistory.py
+++ Code_Review_Assigner/model/commitHistory.py
@@ -3,17 +3,18 @@ class commitHistory(object):
 
     def __init__(self):
         self.commitIDMap = {}
-  
+
 
 class commit(object):
     """description of class"""
 
     def __init__(self, commitID):
         self.commitID = commitID
+        self.timestamp = None
         self.authors = set()
         self.reviewers = set()
         self.filePathsChanged = set()
-        self.previousCommit = set() #set for merge commits
+        self.branches_included_in = set() #set of all branches the given commit is a part of
 
     def getPreviousCommitsInBranch():
         return set() #recursive
