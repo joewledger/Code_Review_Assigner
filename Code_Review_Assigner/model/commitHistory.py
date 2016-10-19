21f765adddfe2fd78c9cbe5aae16eb26d298f884
joewledger <joewledger@gmail.com>
2016-10-18 20:02:44 -0400

    Outlined method to get all previous commits chronologically
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Code_Review_Assigner/model/commitHistory.py
+++ Code_Review_Assigner/model/commitHistory.py
@@ -1,8 +1,17 @@
+from collections import *
+
 class commitHistory(object):
     """description of class"""
 
     def __init__(self):
-        self.commitIDMap = {}
+        #Commits are ordered by their timestamp
+        self.commitIDMap = OrderedDict()
+
+    #Gets all previous commits (as indicated by the commits timestamp). 
+    #If branch_id is specified, only commits that are part of that branch are included
+    def get_previous_commits(commit_id, branch_id=None):
+        return None
+
 
 
 class commit(object):
