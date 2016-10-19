69470879b5c8a670759b148db05c77084ab24ee7
tengchaojiang <jiangtc1988@gmail.com>
2016-10-18 20:25:40 -0400

    reviews only

--- Code_Review_Assigner/controller/code_review_ranking.py
+++ Code_Review_Assigner/controller/code_review_ranking.py
@@ -1,15 +1,39 @@
 from ..model.commitHistory import *
+from .string_compare import *
 
 #                                 branch sensitive?
 #    include          [1. branch,contrib ] [3. !branch,contrib ]
 #    contributors?    [2. !branch,contrib] [4. !branch,!contrib]
 
+commit = Commit
+target_file_paths = commit.filePathsChanged
+
+past_commits = commit.getPreviousCommitsInBranch()
+reviewers = set()
+for target_file_path in target_file_paths:  # for each paths in commit
+    for past_commit in past_commits:  # check all the past commits
+        paths_changed = past_commit.filePathsChanged
+        commit_reviewers = past_commit.reviewers
+        for path in paths_changed:  # for each review's reviewed file path
+            score = string_alignment(target_file_path, path)
+            for commit_reviewer in commit_reviewers:
+                commit_reviewer.score += score
+
 # 1. trace current branch to origin
 #    include contributors/author for weighting
-c = commit
-cps = commit.getPreviousCommitsInBranch()
-for cp in cp:
+commit = Commit
+target_file_paths = commit.filePathsChanged
 
+past_commits = commit.getPreviousCommitsInBranch()
+reviewers = set()
+for target_file_path in target_file_paths:  # for each paths in commit
+    for past_commit in past_commits:  # check all the past commits
+        paths_changed = past_commit.filePathsChanged
+        commit_reviewers = past_commit.reviewers
+        for path in paths_changed:  # for each review's reviewed file path
+            score = string_alignment(target_file_path, path)
+            for commit_reviewer in commit_reviewers:
+                commit_reviewer.score += score
 
 # 2. use all previous commits
 #    include contributors/author for weighting
