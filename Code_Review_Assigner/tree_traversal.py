6788896cc89e15942345e5e92fcff751f0599451
Kevin Candow <kcandow@gmail.com>
2016-10-23 22:47:34 -0400

    fixed bugs in tree traversal for commits with several parent
    the existing tree traversal method would only follow one path to the origin, where several branches may provide equally desirable paths

--- Code_Review_Assigner/tree_traversal.py
+++ Code_Review_Assigner/tree_traversal.py
@@ -1,14 +1,5 @@
-from commitHistroy import *
-
-def tree_traversal(commitHistory, commitID, tree):
-	if commitHistory[commitID].parent is None:
-		return tree
-	for parent in commitHistory[commitID].parents:
-		tree.add(parent.id)
-		tree_traversal(commitHistory, commitHistory[parent.id].parents, tree)
-
-def main():
-   	tree = set()
-   	tree_traversal(commitHistory, commitID, tree)
-
-if __name__ == '__main__': main()
+def tree_traversal(IDmap, commitID, tree):
+    for parent in IDmap[commitID].parents:
+        tree.add(parent)
+        tree = tree_traversal(IDmap, parent, tree)
+    return tree
