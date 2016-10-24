b348bf048f3e5a59fc672d0dbd82e4b30e57a2db
Kevin Candow <kcandow@gmail.com>
2016-10-23 23:48:58 -0400

    modified tester to include get_previous_commits

--- Code_Review_Assigner/Code_Review_Assigner.py
+++ Code_Review_Assigner/Code_Review_Assigner.py
@@ -6,10 +6,12 @@ def main(dirPath=os.getcwd()):
     myCommitHistory = parse(dirPath)
 
     # https://github.com/joewledger/Code_Review_Assigner/commit/61206b44a3511c3252a130144cf3ddaa3ae93029
-    commitID = '61206b44a3511c3252a130144cf3ddaa3ae93029'
+    commitID = 'f8d4bc0cf7cf7f1ccf1525660f2212edae3c8972'
     
-    tree = myCommitHistory.tree_traversal(commitID)
-    print(len(tree))
+    tree = myCommitHistory.get_commits_in_tree(commitID)
+
+    prev = myCommitHistory.get_previous_commits(commitID)
+    print(len(prev))
 
 if __name__ == '__main__': 
     main()
