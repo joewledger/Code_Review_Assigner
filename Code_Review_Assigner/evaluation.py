2f44b74123c6d77a908f73a07898498857b07029
joewledger <joewledger@gmail.com>
2016-11-22 19:59:21 -0500

    Started evaluation code
    Signed-off-by: joewledger <joewledger@gmail.com>

---
+++ Code_Review_Assigner/evaluation.py
@@ -0,0 +1,8 @@
+
+
+def calculate_average_ground_truth_position(repo_name, recommendation_method="", string_compare_method=""):
+	return None
+
+
+def calculate_average_accuracy_for_sliding_window_k(repo_name, recommendation_method = "", string_compare_method="", k_values=list(range(0,10))):
+	return None
