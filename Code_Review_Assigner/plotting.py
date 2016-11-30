1d43f61c84c4fe41c8fa948416f811d485e4335e
joewledger <joewledger@gmail.com>
2016-11-30 13:52:03 -0500

    Wrote code for average accuracy across a sliding window k
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Code_Review_Assigner/plotting.py
+++ Code_Review_Assigner/plotting.py
@@ -21,9 +21,27 @@ def plot_average_position(repo_name, limit=None):
     plt.show()
 
 
-def plot_average_k_acccuracy(repo_name, limit=10, k_values=range(1, 10)):
-    return None
+def plot_average_k_acccuracy(repo_name, limit=None, k_values=range(1, 10)):
+    scenarios = ["scenario%d" % d for d in range(1, 5)]
+    fig, axes = plt.subplots()
+
+    for s in scenarios:
+        print("Now processing: %s" % s)
+
+        sliding_accuracy = evaluation.calculate_average_accuracy_for_sliding_window_k(repo_name, scenario=s, k_values=range(1, 10), limit=limit)
+        sliding_accuracy = [sliding_accuracy[k] for k in k_values]
+
+        axes.plot(k_values, sliding_accuracy)
+
+    axes.set_xlabel("k")
+    axes.set_ylabel("Average Accuracy")
+    axes.set_title("Average accuracy of methods (%s dataset)" % repo_name)
+    axes.set_ylim((0.0, 1.1))
+
+    plt.legend(scenarios)
+    plt.show()
 
 
 if __name__ == "__main__":
     plot_average_position("../help/")
+    plot_average_k_acccuracy("../help/", limit=100)
