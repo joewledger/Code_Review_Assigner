5ba2d3f794b60bfeca5e533c829df71bf805715b
joewledger <joewledger@gmail.com>
2016-11-30 11:21:38 -0500

    Started plotting module
    Signed-off-by: joewledger <joewledger@gmail.com>

---
+++ Code_Review_Assigner/plotting.py
@@ -0,0 +1,12 @@
+
+import matplotlib.pyplot as plt
+
+
+def plot_average_position(repo_name, limit=10):
+	print("h")
+
+def plot_average_k_acccuracy(repo_name, limit=10, k_values=range(1,10))
+
+
+if __name__ == "__main__":
+	plot_average_position("../help/")
