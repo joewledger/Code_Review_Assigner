8085e73d84faaef0aa54fa305e74a323a59f64a9
joewledger <joewledger@gmail.com>
2016-12-02 11:44:22 -0500

    Changed plotting to work on all submodules
    Signed-off-by: joewledger <joewledger@gmail.com>

--- Code_Review_Assigner/plotting.py
+++ Code_Review_Assigner/plotting.py
@@ -3,7 +3,7 @@ import numpy as np
 import evaluation
 
 
-def plot_average_position(repo_name, limit=None):
+def plot_average_position(savefile, repo_name, limit=None):
     scenarios = ["scenario%d" % d for d in range(1, 5)]
 
     fig, axes = plt.subplots()
@@ -14,14 +14,14 @@ def plot_average_position(repo_name, limit=None):
     values = [evaluation.calculate_average_ground_truth_position(repo_name, scenario=s, limit=limit) for s in scenarios]
 
     axes.bar(ind + width / 2, values, width)
-    axes.set_title("Average ranking of ground truth reviewer (%s dataset)" % repo_name)
+    axes.set_title("Average ranking of ground truth reviewer (%s dataset)" % repo_name[:-1])
     axes.set_xticks(ind + width)
     axes.set_xticklabels(tuple(scenarios))
 
-    plt.show()
+    plt.savefig(savefile)
 
 
-def plot_average_k_acccuracy(repo_name, limit=None, k_values=range(1, 10)):
+def plot_average_k_acccuracy(savefile, repo_name, limit=None, k_values=range(1, 10)):
     scenarios = ["scenario%d" % d for d in range(1, 5)]
     fig, axes = plt.subplots()
 
@@ -35,13 +35,16 @@ def plot_average_k_acccuracy(repo_name, limit=None, k_values=range(1, 10)):
 
     axes.set_xlabel("k")
     axes.set_ylabel("Average Accuracy")
-    axes.set_title("Average accuracy of methods (%s dataset)" % repo_name)
+    axes.set_title("Average accuracy of methods (%s dataset)" % repo_name[:-1])
     axes.set_ylim((0.0, 1.1))
 
     plt.legend(scenarios)
-    plt.show()
+    plt.savefig(savefile)
 
 
 if __name__ == "__main__":
-    plot_average_position("../help/")
-    plot_average_k_acccuracy("../help/")
+    submodules = ["qtquickcontrols2", "online", "impress_remote", "help"]
+
+    for s in submodules:
+        plot_average_position("plots/%s_ranking.png" % s, s + "/")
+        plot_average_k_acccuracy("plots/%s_accuracy.png" % s, s + "/")
